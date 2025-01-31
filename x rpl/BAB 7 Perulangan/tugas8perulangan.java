2236280e6a2bf506e6d72034073c640094b6a07b
Dannys Martha Favrillia <166997855+Grimdo1029@users.noreply.github.com>
2025-01-31 10:33:33 +0700

    Add files via upload

---
+++ x rpl/BAB 7 Perulangan/tugas8perulangan.java
@@ -0,0 +1,12 @@
+public class tugas8perulangan {
+    public static void main(String[] args) {
+        // Inisialisasi variabel penghitung
+        int i = 1;
+        
+        // Perulangan while untuk menampilkan kalimat 10 kali
+        while (i <= 10) {
+            System.out.println("Saya berjanji tidak akan mengulangi kesalahan lagi");
+            i++;  // Menambah nilai i agar perulangan berhenti setelah 10 kali
+        }
+    }
+}
